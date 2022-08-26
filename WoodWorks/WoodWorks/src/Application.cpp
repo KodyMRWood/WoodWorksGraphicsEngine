d0fdcfbf91baab41c87dda78c2ddc75f9beefe6d
Kody M.R. Wood <kodymrwood14@gmail.com>
2022-08-26 02:04:22 -0400

    Setting up buffers

--- WoodWorks/WoodWorks/src/Application.cpp
+++ WoodWorks/WoodWorks/src/Application.cpp
@@ -1,3 +1,6 @@
+//Preprocessers
+//#DEFINE GL_STATIC_DRAW onceBut
+
 #include <iostream>
 
 //OpenGL Included Files
@@ -6,27 +9,38 @@
 
 int main()
 {
-        GLFWwindow* window;
+	GLFWwindow* window;
 
-        /* Initialize the library */
-        if (!glfwInit())
-            return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-        /* Create a windowed mode window and its OpenGL context */
-        window = glfwCreateWindow(1080, 720, "Wood Works", NULL, NULL);
-        if (!window)
-        {
-            glfwTerminate();
-            return -1;
-        }
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(1080, 720, "Wood Works", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-        /* Make the window's context current */
-        glfwMakeContextCurrent(window);
-		if (glewInit() != GLEW_OK)
-		{
-			std::cout << "Error GLEW did not Initialize!" << std::endl;
-		}
-		std::cout << glGetString(GL_VERSION) << std::endl;
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Error GLEW did not Initialize!" << std::endl;
+	}
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float m_VertPositions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f };
+
+		unsigned int buffer;
+		glGenBuffers(1, &buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), m_VertPositions, GL_STATIC_DRAW);
+
 
 
         /* Loop until the user closes the window */
@@ -35,11 +49,8 @@ int main()
             /* Render here */
             glClear(GL_COLOR_BUFFER_BIT);
 
-            glBegin(GL_TRIANGLES);
-            glVertex2f(-0.5f, -0.5f);
-            glVertex2f(0.0f, 0.5f);
-            glVertex2f(0.5f, -0.5f);
-            glEnd();
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 3, );
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
