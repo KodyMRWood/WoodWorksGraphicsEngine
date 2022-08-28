08fd3f1460e0cf4f6b59080ccb87f54dc502d7e7
Kody M.R. Wood <kodymrwood14@gmail.com>
2022-08-28 01:29:32 -0400

    Implemented Index Buffers
    Index buffers are arrays that hold vertex indices to help draw polygons more effectively.
    
    Instead of having 6 vertices for a square, the vertex buffer will have 4 vertices with indices that the IndexBuffer will reference 6 times to draw a square
    
    ex:
    0,1,2 // Bottom right triangle of square
    2,3,0 // Upper left triangle of square

--- WoodWorks/WoodWorks/Source/Application.cpp
+++ WoodWorks/WoodWorks/Source/Application.cpp
@@ -116,42 +116,53 @@ int main()
 	}
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float m_VertPositions[9] = {
+	float vertPositions[] = {
 		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.5f,
+		 0.5f, 0.5f,
+		-0.5f, 0.5f,
 	};
 
+	unsigned int indices[] = {
+		0,1,2,
+		2,3,0
+	};
+
+	// Vertex Buffer (A chunk of data that hold vertex data)
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), m_VertPositions, GL_STATIC_DRAW);
-
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2* sizeof(float), vertPositions, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	// Index Buffer (A chunk of data that hold the index of vertices)
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	ShaderProgramSource source = ParseShader("Resources/Shader/Basic.shader");
 	std::cout << "VERTEX" << std::endl;
 	std::cout << source.VertexSource << std::endl;
 	std::cout << "FRAGMENT" << std::endl;
 	std::cout << source.FragmentSource << std::endl;
-
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
        /* Loop until the user closes the window */
        while (!glfwWindowShouldClose(window))
        {
-           /* Render here */
-           glClear(GL_COLOR_BUFFER_BIT);
+			/* Render here */
+			glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
-           /* Swap front and back buffers */
-           glfwSwapBuffers(window);
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
 
-           /* Poll for and process events */
-           glfwPollEvents();
+			/* Poll for and process events */
+			glfwPollEvents();
        }
 
 	   //Deletes the shaders being used for clean up
